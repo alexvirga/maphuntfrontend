bbe4250e38b8565940c2fd76a71fb5deb608fd44
adriennem76 <adrienne.c.miranda@gmail.com>
2019-08-20 14:02:20 -0400

    styling started

--- public/src/index.js
+++ public/src/index.js
@@ -3,9 +3,11 @@ const mykey = config.MY_KEY;
 document.addEventListener("DOMContentLoaded", () => {
     document.body.insertAdjacentHTML("afterbegin", 
 `<iframe class="column left" id="map"
-  src="Https://www.google.com/maps/embed/v1/streetview?key=${mykey}&location=40.705258,-74.0138061&heading=210&pitch=10&fov=35" allowfullscreen>
+  src="" allowfullscreen>
 </iframe>`)
 })
 
 
 console.log(document.getElementsByClassName("gm-iv-short-address-description")[0].innerText)
+
+// "Https://www.google.com/maps/embed/v1/streetview?key=${mykey}&location=40.705258,-74.0138061&heading=210&pitch=10&fov=35"
