0e816bcb2f973921bb56235f33450c468aae506b
alexvirga <alexander.virga@gmail.com>
2019-08-20 09:26:37 -0400

    working on map listener

--- public/src/index.js
+++ public/src/index.js
@@ -1,2 +1,11 @@
 const mykey = config.MY_KEY;
-console.log(mykey)
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.insertAdjacentHTML("afterbegin", 
+`<iframe class="column left" id="map"
+  src="Https://www.google.com/maps/embed/v1/streetview?key=${mykey}&location=40.705258,-74.0138061&heading=210&pitch=10&fov=35" allowfullscreen>
+</iframe>`)
+})
+
+
+console.log(document.getElementsByClassName("gm-iv-short-address-description")[0].innerText)
