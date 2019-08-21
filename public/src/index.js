9151c8975a6890af02a737e5feb75347f3d59b1a
alexvirga <alexander.virga@gmail.com>
2019-08-21 01:27:38 -0400

    Altered code for finding item

--- public/src/index.js
+++ public/src/index.js
@@ -17,56 +17,76 @@ function initPano() {
         setTimeout(loadAddress, 100)
     });
  
-    function loadAddress(){
-        let currentLocation = document.querySelector("#pano > div > div:nth-child(10) > div.gm-iv-address > div.gm-iv-address-description > div.gm-iv-short-address-description")
-        let locationContent = currentLocation.textContent
-        verifyAnswer(locationContent)
-    }
+
 
 }
+
+function loadAddress(){
+  let currentLocation = document.querySelector("#pano > div > div:nth-child(10) > div.gm-iv-address > div.gm-iv-address-description > div.gm-iv-short-address-description")
+  let locationContent = currentLocation.textContent
+  verifyAnswer(locationContent)
+}
+
 let items = [
     {
       name: "Flatiron School",
-      address: "20 Canyon of Heroes",
+      address: "22 Canyon of Heroes",
       img: " "
-    },
-    {
-        name: "New York Stock Exchange",
-        address: "11 Wall St",
-        img: " "
     },
     {
         name: "Big Red Cube",
         address: "135 Broadway",
         img: " "
-    }
+    },
+    {
+      name: "Bull",
+      address: "27 Canyon of Heroes",
+      img: " "
+  },
+  {
+    name: "DMV",
+    address: "3 Greenwich St",
+    img: " "
+},
+{
+  name: "New York Stock Exchange",
+  address: "11 Wall St",
+  img: " "
+},
   ];
 
 
 function drawCards(){
 let newCard = document.getElementById("flex-container")
-
-
-   for (let i = 0; i < 5; i++){
-    let randomLocation = items[Math.floor(Math.random() * items.length)];
+  //  for (let i = 0; i < 5; i++){
+    // let randomLocation = items[Math.floor(Math.random() * items.length)];
+    items.forEach(function(randomLocation){
     newCard.insertAdjacentHTML("beforeend",
     `<div class="card border-0 shadow location-card" id="${randomLocation.address}">
     <img src="imgs/NYSE.png" class="card-img-top" alt="...">
     <div class="card-body text-center">
       <h5 class="card-title mb-0">${randomLocation.name}</h5>
     </div>
-  </div>`)}
+  </div>`)})
 }
 
 function verifyAnswer(address){
 let cardAddress = document.getElementsByClassName("card border-0 shadow location-card")
 let i = 0
-if(cardAddress[i].id !== address ){
-    i++
+if(cardAddress[0].id === address ){
+  cardAddress[0].style.backgroundColor = "grey"}
+    else if (cardAddress[1].id === address ){
+      cardAddress[1].style.backgroundColor = "grey"}
+    else if (cardAddress[2].id === address ){
+      cardAddress[2].style.backgroundColor = "grey"}
+    else if (cardAddress[3].id === address ){
+      cardAddress[3].style.backgroundColor = "grey"}
+    else if (cardAddress[4].id === address ){
+      cardAddress[4].style.backgroundColor = "grey"}
 }
-else
-cardAddress[i].style.backgroundColor = "grey"
-}
+
+
+
 
   initPano()
   drawCards()
